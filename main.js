9e90088ea51164f63994b3670c4b2ccf8cea392c
Shlomi Shoshani <shlomi.shosha@gmail.com>
2023-12-21 07:14:00 +0200

    test function in main

--- main.js
+++ main.js
@@ -22,7 +22,28 @@ app.get('/', (req, res) => {
 });
 
 app.post('/test', (req, res) => {
-  res.send('kaki kaki kaki');
+  console.log('test');
+  let imageFile = req.files.uploadedFile;
+  if (imageFile.mimetype === 'image/jpeg' || imageFile.mimetype === 'image/png') {
+    try {
+      let time = new Date().getTime();
+      newImageType = imageFile.name.replace('.jpg', '.png');
+      let fileNameAndUploadedTime = time + '_' + newImageType;
+
+      imageFile.mv(`${__dirname}/../uploaded_images/${fileNameAndUploadedTime}`, (err) => {
+        if (err) {
+          res.status(400).send(err);
+        } else {
+          removeBgFunc(fileNameAndUploadedTime);
+          res.status(201).send(fileNameAndUploadedTime);
+        }
+      });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } else {
+    res.status(415).json({ errMsg: 'Unsupported file' });
+  }
 });
 
 app.listen(port, () => {
