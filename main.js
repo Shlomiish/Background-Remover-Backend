febcb9bf28b073cf97137a0c4163bd1c189f828e
Shlomi Shoshani <shlomi.shosha@gmail.com>
2023-12-21 11:07:40 +0200

    add test log

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ app.post('/test', (req, res) => {
           res.status(400).send(err);
           console.log('test5');
         } else {
+          console.log('test6');
           removeBgFunc(fileNameAndUploadedTime);
           res.status(201).send(fileNameAndUploadedTime);
         }
