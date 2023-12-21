a9f8d8fdb60fe437546c47954555ae8fdd26d4b7
Shlomi Shoshani <shlomi.shosha@gmail.com>
2023-12-21 14:22:24 +0200

    change

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ app.post('/test', (req, res) => {
     newImageType = imageFile.name.replace('.jpg', '.png');
     let fileNameAndUploadedTime = time + '_' + newImageType;
     res.send(fileNameAndUploadedTime);
-    imageFile.mv(`${__dirname}`, (err) => {
+    imageFile.mv(`${__dirname}/myapp`, (err) => {
       if (err) {
         res.status(400).send(err);
       } else {
